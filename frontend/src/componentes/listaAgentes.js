6c6d04cae36ae60b47acee4689450dbb31746e0c
Alberto Zaranza <albertozaranza@outlook.com>
2019-02-25 23:21:07 -0300

    Busca na lista de agentes

--- frontend/src/componentes/listaAgentes.js
+++ frontend/src/componentes/listaAgentes.js
@@ -7,7 +7,8 @@ export default class ListaAgentes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            agentes: null
+            agentes: null,
+            search: ''
         };
     }
     componentWillMount() {
@@ -20,19 +21,41 @@ export default class ListaAgentes extends Component {
         })
     }
     renderItems = () => {
-        return this.state.agentes.map((item, index) => (
-            <li key={index} className="list-group-item rounded m-1">
-                <BotaoAgente id={item.id}/>
-            </li>
-        ))
+        let agentesFiltrados = this.state.agentes.filter(agente => {
+            return agente.id.indexOf(this.state.search) !== -1
+        })
+        if(agentesFiltrados.length == 0){
+            return 'Nenhum resultado foi encontrado'
+        }
+        else {
+            return agentesFiltrados.map((item, index) => (
+                <li key={index} className="list-group-item rounded m-1">
+                    <BotaoAgente id={item.id}/>
+                </li>
+            ))
+        }
+        
+    }
+    searchHandler = event => {
+        this.setState({search: event.target.value.substr(0, 20)})
     }
     render() {
         if(this.state.agentes != null)
             return (
-                <div className="p-2 m-2 rounded border" style={{backgroundColor: '#F8F8F8'}}>
-                    <ul className="list-group">
-                        {this.renderItems()}
-                    </ul>
+                <div>
+                    <div>
+                        <div className="p-2 m-2 rounded border d-flex d-column align-items-center" style={{backgroundColor: '#F8F8F8'}}>
+                            <i class="fa fa-search ml-3 mr-3" aria-hidden="true"></i>
+                            <input className='form-control rounded m-1' placeholder='Buscar agente' 
+                                onChange={this.searchHandler}
+                                value={this.state.search}/>
+                        </div>
+                    </div>
+                    <div className="p-2 m-2 rounded border" style={{backgroundColor: '#F8F8F8'}}>
+                        <ul className="list-group">
+                            {this.renderItems()}
+                        </ul>
+                    </div>
                 </div>
             )
         else
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -11,7 +11,7 @@ export default class Routes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            auth: false
+            auth: true
         };
     }
     render() {
