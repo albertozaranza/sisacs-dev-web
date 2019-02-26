902a5d222eb3b0e64e5a9277db8bfdee21c9da38
Alberto Zaranza <albertozaranza@outlook.com>
2019-02-26 01:54:17 -0300

    Identificação do usuário logado

--- frontend/src/routes/home.js
+++ frontend/src/routes/home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../componentes/header'
+import firebase from 'firebase'
 
 export default class Menu extends Component {
   render() {
     return (
         <div>
-            <Header name="Bem vindo"/>
+            <Header name={`Bem vindo, ${firebase.auth().currentUser.email}`}/>
             <Link to={'/agentes'}>
               <button>Form1</button>
             </Link>
