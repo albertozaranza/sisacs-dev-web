39b4343121042d5b2a9a20dcab5d5b99c1a22d8d
albertozaranza <albertozaranza@outlook.com>
2019-03-27 15:39:12 -0300

    Reestruturação do projeto

--- backend/express/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const AdminSchema = new mongoose.Schema({
-    email: String,
-    senha: String
-});
-
-module.exports = mongoose.model('Admin' , AdminSchema)
--- backend/express/models/Digitador.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const DigitadorSchema = new mongoose.Schema({
-    email: String,
-    senha: String,
-    nome: String
-});
-
-module.exports = mongoose.model('Digitador' , DigitadorSchema)
